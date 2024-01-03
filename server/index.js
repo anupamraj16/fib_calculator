d53eef62df83240fc15a26cfe2b53921cf46e1a6
anupamraj16 <72901179+anupamraj16@users.noreply.github.com>
2024-01-03 00:29:21 -0700

    working state

--- server/index.js
+++ server/index.js
@@ -1,46 +1,79 @@
-const keys = require('./keys')
+import {
+  redisHost,
+  redisPort,
+  pgUser,
+  pgHost,
+  pgDatabase,
+  pgPassword,
+  pgPort,
+} from './keys.js'
 
 // Express App Setup
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-const router = require('./routes')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5001
-
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 // Postgres Client Setup
-const { Pool } = require('pg')
-const pgClient = new Pool({
-  user: keys.pgUser,
-  host: keys.pgHost,
-  database: keys.pgDatabase,
-  password: keys.pgPassword,
-  port: keys.pgPort,
+import Client from 'pg'
+const pgClient = new Client.Client({
+  user: pgUser,
+  host: pgHost,
+  database: pgDatabase,
+  password: pgPassword,
+  port: pgPort,
+  ssl:
+    process.env.NODE_ENV !== 'production'
+      ? false
+      : { rejectUnauthorized: false },
 })
-
-pgClient.on('connect', (client) => {
-  client
+pgClient.connect((err) => {
+  pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch((err) => console.error(err))
 })
 
 // Redis Client Setup
-const redis = require('redis')
-const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
+import { createClient } from 'redis'
+const redisClient = createClient({
+  url: `${redisHost}://${redisHost}:${redisPort}`,
   retry_strategy: () => 1000,
 })
-const redisPublisher = redisClient.duplicate()
+redisClient.on('error', (err) => console.log('Redis Client Error', err))
+await redisClient.connect()
+const subscriber = redisClient.duplicate()
+subscriber.on('error', (err) => console.error(err))
+await subscriber.connect()
 
 // Express route handlers
-app.use('/', router)
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values')
+
+  res.send(values.rows)
+})
+
+app.get('/values/current', async (req, res) => {
+  const values = await redisClient.hGetAll('values')
+  res.send(values)
+})
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high')
+  }
+
+  redisClient.hSet('values', index, 'Nothing yet!')
+  redisClient.publish('insert', index)
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+
+  res.send({ working: true })
+})
 
 app.listen(PORT, (err) => {
-  console.log(`Listening on Port ${PORT}...`)
+  console.log(`Listening on port ${PORT}`)
 })
--- worker/index.js
+++ worker/index.js
@@ -1,19 +1,22 @@
-const keys = require('./keys')
-const redis = require('redis')
-
-const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
+import { redisHost, redisPort } from './keys.js'
+import { createClient } from 'redis'
+const redisClient = createClient({
+  url: `${redisHost}://${redisHost}:${redisPort}`,
   retry_strategy: () => 1000,
 })
-const sub = redisClient.duplicate()
+redisClient.on('error', (err) => console.log('Redis Client Error', err))
+await redisClient.connect()
+const subscriber = redisClient.duplicate()
+await subscriber.connect()
 
 function fib(index) {
   if (index < 2) return 1
   return fib(index - 1) + fib(index - 2)
 }
 
-sub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fib(parseInt(message)))
-})
-sub.subscribe('insert')
+const listener = (message, channel) => {
+  const fibValue = fib(parseInt(message))
+  redisClient.hSet('values', message, fibValue)
+}
+
+await subscriber.subscribe('insert', listener)
